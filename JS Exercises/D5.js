6d394a3c0ad55d8b7ae858ff230d70a1d24fc149
Daniel Rrapi <uzo1209@gmail.com>
2023-10-06 18:01:56 +0200

    second commit

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -64,6 +64,10 @@ const cars = [
 */
 const justTrims = []
 
+for(let i = 0; i < cars.length ; i++){
+  justTrims.push(cars[i].trims[0])
+}
+
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
